60a2292c940ec81ab9d77ea8432235c0ced29c94
AdryCU <74635617+AdryCU@users.noreply.github.com>
2021-06-10 16:34:40 -0500

    Adriana no estuvo aquí :3

--- app/src/main/java/com/classhub/agendapp/MainActivity.java
+++ app/src/main/java/com/classhub/agendapp/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hola buenas tardes
+
+        Toast.makeText(this, "Adriana estuvo aquí :3", Toast.LENGTH_SHORT).show();
 
     }
 }
