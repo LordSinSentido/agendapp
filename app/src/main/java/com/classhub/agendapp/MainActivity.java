51b2f9e55d6291d9fad3659c85205028f296ade2
Saitama18 <a19310167@ceti.mx>
2021-06-10 12:24:37 -0500

    Estoy probando el GitHub

--- app/src/main/java/com/classhub/agendapp/MainActivity.java
+++ app/src/main/java/com/classhub/agendapp/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(this, "Hola mundooooooooo!!!!!!!1 :D", Toast.LENGTH_SHORT).show();
+        //Hola buenas tardes
 
     }
 }
