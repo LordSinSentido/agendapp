187373e7cf53eb9b58f0f0b2cd1891d6567b1918
Saitama18 <a19310167@ceti.mx>
2021-06-12 23:27:38 -0500

    Se agrego la funcionalidad a la activity del menu principal

--- app/src/main/java/com/classhub/agendapp/MenuPrincipalActivity.java
+++ app/src/main/java/com/classhub/agendapp/MenuPrincipalActivity.java
@@ -10,11 +10,50 @@ import com.google.android.material.tabs.TabLayout;
 public class MenuPrincipalActivity extends AppCompatActivity {
     private TabLayout pestannas;
     private ViewPager actividades;
-
+    ControladorMenuPestannas controlador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        pestannas = findViewById(R.id.menuprincipalTabLayoutPestannas);
+        actividades = findViewById(R.id.menuprincipalViewPagerActividades);
+
+        controlador = new ControladorMenuPestannas(getSupportFragmentManager(), pestannas.getTabCount());
+        actividades.setAdapter(controlador);
+
+        pestannas.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                actividades.setCurrentItem(tab.getPosition());
+
+                switch (tab.getPosition()){
+                    case 0:
+                        controlador.notifyDataSetChanged();
+                        break;
+                    case 1:
+                        controlador.notifyDataSetChanged();
+                        break;
+                    case 2:
+                        controlador.notifyDataSetChanged();
+                        break;
+                    case 3:
+                        controlador.notifyDataSetChanged();
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        actividades.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(pestannas));
     }
 }
