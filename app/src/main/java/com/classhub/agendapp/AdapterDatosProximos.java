18fa202b89e07dc2fa2ed45f61abe90a4df7ba68
LordSinSentido <misogu@live.com>
2021-06-16 11:42:29 -0500

    Se corrigio el estilo de los items de las listas

--- app/src/main/java/com/classhub/agendapp/AdapterDatosProximos.java
+++ app/src/main/java/com/classhub/agendapp/AdapterDatosProximos.java
@@ -3,7 +3,6 @@ package com.classhub.agendapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,7 +24,7 @@ public class AdapterDatosProximos extends RecyclerView.Adapter<AdapterDatosProxi
     @Override
     public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_list_proximo_layout, null, false);
+                .inflate(R.layout.vista_lista, null, false);
         view.setOnClickListener(this);
         return new ViewHolderDatos(view);
     }
