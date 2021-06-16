bc025f3287a7e69b5d5e340509114d3a2586374a
LordSinSentido <misogu@live.com>
2021-06-16 01:09:53 -0500

    Se agrego la funcionalidad de base de datos

--- app/src/main/java/com/classhub/agendapp/BaseDeDatosControlador.java
+++ app/src/main/java/com/classhub/agendapp/BaseDeDatosControlador.java
@@ -8,8 +8,6 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-import com.google.android.material.snackbar.Snackbar;
-
 public class BaseDeDatosControlador extends SQLiteOpenHelper {
     public BaseDeDatosControlador(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -17,14 +15,15 @@ public class BaseDeDatosControlador extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase baseDeDatos) {
-        String crearTareas = "create table tareas (id int primary key autoincrement not null, titulo text not null, descripcion text, prioridad int not null, tipo text not null, recordatorio boolean not null, fechaDeFin date not null, horaDeFin time not null)";
-        String crearHorario = "create table horario (id int primary key autoincrement not null, titulo text not null, descripcion text, prioridad int not null, tipo text not null, recordatorio boolean not null, fechaDeFin date not null, horaDeFin time not null, ubicacion text, horaDeInicio time not null)";
-        String crearEvento = "create table horario (id int primary key autoincrement not null, titulo text not null, descripcion text, prioridad int not null, tipo text not null, recordatorio boolean not null, fechaDeFin date not null, horaDeFin time not null, ubicacion text, horaDeInicio time not null, fechaDeInicio date not null)";
+        String crearTareas = "create table tareas (id integer primary key autoincrement not null, titulo text not null, descripcion text, prioridad int not null, tipo text not null, recordatorio text, fechaDeFin date not null, horaDeFin time not null)";
+        String crearHorario = "create table horario (id integer primary key autoincrement not null, titulo text not null, descripcion text, prioridad int not null, tipo text not null, recordatorio text, fechaDeFin date not null, horaDeFin time not null, ubicacion text, horaDeInicio time not null)";
+        String crearEvento = "create table evento (id integer primary key autoincrement not null, titulo text not null, descripcion text, prioridad int not null, tipo text not null, recordatorio text, fechaDeFin date not null, horaDeFin time not null, ubicacion text, horaDeInicio time not null, fechaDeInicio date not null)";
 
         try {
             baseDeDatos.execSQL(crearTareas);
             baseDeDatos.execSQL(crearHorario);
             baseDeDatos.execSQL(crearEvento);
+
         } catch (SQLException e) {
             Toast.makeText(null, "Error: " + e.toString(), Toast.LENGTH_SHORT).show();
         }
