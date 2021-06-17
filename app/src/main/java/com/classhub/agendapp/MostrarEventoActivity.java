2071d6ccd4b4515639986d1587eaafa1f971594a
Saitama18 <a19310167@ceti.mx>
2021-06-16 22:41:52 -0500

    Se agrego los accesos a las activities de vista de datos

--- app/src/main/java/com/classhub/agendapp/MostrarEventoActivity.java
+++ app/src/main/java/com/classhub/agendapp/MostrarEventoActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
@@ -28,4 +29,7 @@ public class MostrarEventoActivity extends AppCompatActivity {
         Snackbar.make(this, view, "Si funciona", BaseTransientBottomBar.LENGTH_LONG).show();
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
