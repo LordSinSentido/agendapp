d3312bfb2aebce8b63bfb70c1e79f7c24d743cad
LordSinSentido <misogu@live.com>
2021-06-16 23:17:34 -0500

    Se agrego la vista de eventos

--- app/src/main/java/com/classhub/agendapp/EventosFragment.java
+++ app/src/main/java/com/classhub/agendapp/EventosFragment.java
@@ -1,12 +1,19 @@
 package com.classhub.agendapp;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +30,11 @@ public class EventosFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    RecyclerView recyclerEventos;
+    ArrayList<ActividadDatos> listaActividades;
+    BaseDeDatosControlador admin;
+    int cantidad = 0;
 
     public EventosFragment() {
         // Required empty public constructor
@@ -58,7 +70,45 @@ public class EventosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eventos, container, false);
+        View vista = inflater.inflate(R.layout.fragment_eventos, container, false);
+
+        admin = new BaseDeDatosControlador(getContext(), "baseDeDatos.db", null, 1);
+
+        ActividadDatos actividadDatos = null;
+        listaActividades = new ArrayList<>();
+        recyclerEventos = vista.findViewById(R.id.recyclerIdEventos);
+        recyclerEventos.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+
+        SQLiteDatabase db = admin.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM eventos order by fechaDeFin asc, horaDeFin asc", null);
+
+        cantidad = cursor.getCount();
+
+        if(cantidad>0){
+            cursor.moveToFirst();
+            do{
+                actividadDatos = new ActividadDatos();
+                actividadDatos.setTitulo(cursor.getString(1));
+                actividadDatos.setDescripcion(cursor.getString(2));
+                actividadDatos.setImagen(R.drawable.icono_tareas);
+                listaActividades.add(actividadDatos);
+            }while(cursor.moveToNext());
+        }
+        else
+            Toast.makeText(getContext(), "No se encontraron registros.", Toast.LENGTH_SHORT).show();
+        db.close();
+
+        AdapterDatosProximos adpater = new AdapterDatosProximos(listaActividades);
+        adpater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),
+                        "Abriendo: " + listaActividades.get(recyclerEventos.getChildAdapterPosition(view)).getTitulo(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerEventos.setAdapter(adpater);
+
+        return vista;
     }
 }
