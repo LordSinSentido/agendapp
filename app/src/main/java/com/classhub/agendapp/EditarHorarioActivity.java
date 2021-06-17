778ca8b4465a818a865508c584a2c213e1964d74
Saitama18 <a19310167@ceti.mx>
2021-06-17 18:02:15 -0500

    Se agregaron las vistas de los activities editar tareas y editar horario

---
+++ app/src/main/java/com/classhub/agendapp/EditarHorarioActivity.java
@@ -0,0 +1,14 @@
+package com.classhub.agendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarHorarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_horario);
+    }
+}
---
+++ app/src/main/java/com/classhub/agendapp/EditarTareaActivity.java
@@ -0,0 +1,14 @@
+package com.classhub.agendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarTareaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_tarea);
+    }
+}
