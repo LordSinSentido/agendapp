36d8107a6fe3a26bda4d144aad9581c94cae9b61
Saitama18 <a19310167@ceti.mx>
2021-06-16 22:40:03 -0500

    Se agrego los accesos a las activities de vista de datos

--- app/src/main/java/com/classhub/agendapp/ConfiguracionesActivity.java
+++ app/src/main/java/com/classhub/agendapp/ConfiguracionesActivity.java
@@ -2,7 +2,12 @@ package com.classhub.agendapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.InputStreamReader;
 
 public class ConfiguracionesActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class ConfiguracionesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuraciones);
+    }
+
+    public void Evento (View view){
+        Intent intent = new Intent(this, MostrarEventoActivity.class);
+        startActivity(intent);
     }
 }
