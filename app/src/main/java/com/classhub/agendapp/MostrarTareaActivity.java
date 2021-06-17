d6febd398206fb7fb5eeff6fd8a0a903a64d2dcb
LordSinSentido <misogu@live.com>
2021-06-16 22:39:12 -0500

    Se agrego los accesos a las activities de vista de datos

---
+++ app/src/main/java/com/classhub/agendapp/MostrarTareaActivity.java
@@ -0,0 +1,31 @@
+package com.classhub.agendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MostrarTareaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_tarea);
+    }
+
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.vista_mostrardatos, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected (MenuItem item) {
+        View view = new View(this,null);
+        Snackbar.make(this, view,"Si funciona", BaseTransientBottomBar.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
+    }
+}
