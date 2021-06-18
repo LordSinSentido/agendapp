dcc99f6ac46ba614ef779d4b3e0af41e59755c31
LordSinSentido <misogu@live.com>
2021-06-17 21:05:41 -0500

    Se agrego tiempo de espera para la splashscreen

--- app/src/main/java/com/classhub/agendapp/MainActivity.java
+++ app/src/main/java/com/classhub/agendapp/MainActivity.java
@@ -6,15 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("");
 
-        Intent intent = new Intent(this, MenuPrincipalActivity.class);
-        startActivity(intent);
-        finish();
+        TimerTask actividad = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MenuPrincipalActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer espera = new Timer();
+        espera.schedule(actividad, 2000);
     }
 }
